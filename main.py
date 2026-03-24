62e61ea2a00566a8c700abaf2c1ea12bc47c4137
Đorđe Grabovičanović <djgrabovicanovic2024.20055@politehnika.edu.rs>
2026-03-24 18:16:39 +0100

    Dodati komentari osnovne strukture programa

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
+
+# Glavni modul aplikacije
 class App:
     def __init__(self):
-        self.routes = {}
-        self.proizvodi = []
+        self.routes = {}    # mapa komandi i njihovih funkcija
+        self.proizvodi = [] # lista proizvoda
         
+        # Definisanje ruta
+
         self.routes["add"] = {
             "desc": "Dodaj novi zapis",
             "func": add_proizvod
@@ -14,9 +18,11 @@ class App:
             "func": show_proizvodi
         }
 
+    # Glavna petlja aplikacije
     def run(self):
         while True:
 
+            # Prikaz dostupnih komandi
             print("Dostupne komande:")
             for cmd, info in self.routes.items():
                 print(f"{cmd} - {info['desc']}")
@@ -24,16 +30,20 @@ class App:
             print("exit - Izlaz iz programa")
             print()
 
+            # Čitanje korisničkog unosa
             cmd = input("> ").strip()
+
+            # Obrada komande
             if cmd == "exit":
                 break
 
             if cmd in self.routes:
+                # Pozivanje funkcije povezane sa komandom
                 self.routes[cmd]["func"]()
             else:
                 print("Nepoznata komanda. Pokušajte ponovo.")
 
-
+# Funkcija za dodavanje novog proizvoda
 def add_proizvod():
     naziv =input("Naziv proizvoda> ").strip()
     cena = float(input("Cena proizvoda> ").strip())
@@ -45,11 +55,13 @@ def add_proizvod():
     }
     app.proizvodi.append(proizvod)
 
+# Funkcija za prikaz svih proizvoda
 def show_proizvodi():
     for proizvod in app.proizvodi:
         print(f"Naziv: {proizvod['naziv']}, Cena: {proizvod['cena']}, Proizvođač: {proizvod['proizvodjac']}")
     print()
 
 if __name__ == "__main__":
+    # Inicijalizacija i pokretanje aplikacije
     app = App()
     app.run()
