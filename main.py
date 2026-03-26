96e5dc0e5ca45b719c236cd2b61f82e0f38870fb
Đorđe Grabovičanović <djgrabovicanovic2024.20055@politehnika.edu.rs>
2026-03-26 21:34:27 +0100

    Iz kontrolera za proizvod izdvojene klase za repozitorij i interakciju sa konzolom

--- main.py
+++ main.py
@@ -75,12 +75,76 @@ class ProizvodController:
 
     def __init__(self, app):
         self.app = app
-        self.proizvodi = [] # lista proizvoda
-        self.current_proizvod_id = 0  ## TODO move to model class
+        self.proizvodRepository = ProizvodRepository()
+        
 
     # Funkcija za dodavanje novog proizvoda
     def add_proizvod(self):
-        naziv =input("Naziv proizvoda> ").strip()
+        naziv, cena, proizvodjac = Proizvodcli.input_proizvod()
+        id = self.proizvodRepository.add_proizvod(naziv, cena, proizvodjac)
+        Proizvodcli.log(f"Proizvod ID: {id} je uspešno dodat.")
+
+    # Funkcija za prikaz svih proizvoda
+    def show_proizvodi(self):
+        proizvodi = self.proizvodRepository.all_proizvodi()
+        if not proizvodi:
+            Proizvodcli.log("Nema proizvoda za prikaz.")
+            return
+        
+        for proizvod in proizvodi:
+            Proizvodcli.log(f"ID: {proizvod['id']}, Naziv: {proizvod['naziv']}, Cena: {proizvod['cena']}, Proizvođač: {proizvod['proizvodjac']}")
+        
+
+    # Funkcija za prikaz detalja jednog proizvoda
+    def show_detalji(self):
+        try:
+            id_proizvoda = Proizvodcli.input_id()
+            proizvod = self.proizvodRepository.get_proizvod_by_id(id_proizvoda)
+            if proizvod:
+                Proizvodcli.output_proizvod(proizvod)
+        except ValueError as e:
+            Proizvodcli.log(str(e))
+    
+    # Funkcija za brisanje proizvoda
+    def delete_proizvod(self):
+        try:
+            id_proizvoda = Proizvodcli.input_id()
+            self.proizvodRepository.delete_proizvod(id_proizvoda)
+            Proizvodcli.log("Proizvod je uspešno obrisan.")
+        except ValueError as e:
+            Proizvodcli.log(str(e))
+            return
+
+    # Funkcija za ažuriranje proizvoda
+    def update_proizvod(self):
+        try:
+            id_proizvoda = Proizvodcli.input_id()
+            proizvod = self.proizvodRepository.get_proizvod_by_id(id_proizvoda)
+            naziv, cena, proizvodjac = Proizvodcli.edit_val_proizvod(proizvod)
+            self.proizvodRepository.update_proizvod(id_proizvoda, naziv, cena, proizvodjac)
+            Proizvodcli.log("Proizvod je uspešno ažuriran.")
+        except ValueError as e:
+            Proizvodcli.log(str(e))
+            return
+
+    # Sačuvaj podatke u fajl
+    def save_data(self, file="proizvodi.txt"):
+        self.proizvodRepository.save_proizvodi(file)
+
+    # Učitaj podatke iz fajla
+    def load_data(self, file="proizvodi.txt"):
+        self.proizvodRepository.load_data(file)
+
+class Proizvodcli:
+    
+    # Ispisivanje stringa na konzoli i možda u fajl
+    @staticmethod
+    def log(logstring):
+        print(logstring)
+
+    @staticmethod
+    def input_proizvod():
+        naziv = input("Naziv proizvoda> ").strip()
         while True:
             try:
                 cena = float(input("Cena proizvoda> ").strip())
@@ -88,6 +152,57 @@ class ProizvodController:
             except ValueError:
                 print("Greška: Unesite validan broj za cenu.")
         proizvodjac = input("Proizvođač> ").strip()
+        return naziv, cena, proizvodjac
+
+    @staticmethod
+    def input_id():
+        while True:
+            try:
+                return int(input("Unesite ID proizvoda> ").strip())
+            except ValueError:
+                print("Greška: Unesite validan broj za ID.")
+
+    @staticmethod
+    def output_proizvod(proizvod):
+        print(f"ID: {proizvod['id']}")
+        print(f"Naziv: {proizvod['naziv']}")
+        print(f"Cena: {proizvod['cena']}")
+        print(f"Proizvođač: {proizvod['proizvodjac']}")
+    
+    @staticmethod
+    def edit_val_proizvod(proizvod):
+        if not proizvod:
+            raise ValueError("Proizvod nije prosleđen")
+        
+        novi_naziv = input(f"Unesite novi naziv ({proizvod['naziv']})> ").strip()
+        if not novi_naziv:
+            novi_naziv = proizvod['naziv']
+        
+        nova_cena = proizvod['cena']
+        while True:
+            cena = input(f"Unesite novu cenu ({proizvod['cena']})> ").strip()
+            if not cena:
+                break
+            else:
+                try:
+                    nova_cena = float(cena)
+                    break
+                except ValueError:
+                    Proizvodcli.log("Greška: Cena nije ažurirana jer uneta vrednost nije validan broj.")
+
+        novi_proizvodjac = input(f"Unesite novog proizvođača ({proizvod['proizvodjac']})> ").strip()
+        if not novi_proizvodjac:
+            novi_proizvodjac = proizvod['proizvodjac']
+        return novi_naziv, nova_cena, novi_proizvodjac
+        
+
+class ProizvodRepository:
+    def __init__(self):
+        self.proizvodi = [] # lista proizvoda
+        self.current_proizvod_id = 0
+    
+    def add_proizvod(self, naziv, cena, proizvodjac):
+        
         proizvod = {
             "id": self.current_proizvod_id + 1,
             "naziv": naziv,
@@ -95,96 +210,38 @@ class ProizvodController:
             "proizvodjac": proizvodjac
         }
         self.proizvodi.append(proizvod)
-        print("Proizvod je uspešno dodat.")
         self.current_proizvod_id = self.current_proizvod_id + 1
+        return proizvod['id']
 
-    # Funkcija za prikaz svih proizvoda
-    def show_proizvodi(self):
-        if not self.proizvodi:
-            print("Nema proizvoda za prikaz.")
-            return
-        
-        for proizvod in self.proizvodi:
-            print(f"ID: {proizvod['id']}, Naziv: {proizvod['naziv']}, Cena: {proizvod['cena']}, Proizvođač: {proizvod['proizvodjac']}")
-        print()
+    def all_proizvodi(self):
+        return list(self.proizvodi)
 
-    # Funkcija za prikaz detalja jednog proizvoda
-    def show_detalji(self):
-        try:
-            id_proizvoda = int(input("Unesite ID proizvoda> ").strip())
-        except ValueError:
-            print("Greška: Unesite validan broj za ID.")
-            return
+    def get_proizvod_by_id(self, id):
         for proizvod in self.proizvodi:
-            if proizvod["id"] == id_proizvoda:
-                print(f"ID: {proizvod['id']}")
-                print(f"Naziv: {proizvod['naziv']}")
-                print(f"Cena: {proizvod['cena']}")
-                print(f"Proizvođač: {proizvod['proizvodjac']}")
-                return
-        print(f"Proizvod - ID {id_proizvoda} nije pronađen.")
-        
-    # Funkcija za brisanje proizvoda
-    def delete_proizvod(self):
-        try:
-            id_proizvoda = int(input("Unesite ID proizvoda za brisanje> ").strip())
-        except ValueError:
-            print("Greška: Unesite validan broj za ID.")
-            return
-        
+            if proizvod["id"] == id:
+                return proizvod
+        raise ValueError(f"Proizvod - ID {id} nije pronađen.")
+    
+    def delete_proizvod(self, id):
         for proizvod in self.proizvodi:
-            if proizvod["id"] == id_proizvoda:
+            if proizvod["id"] == id:
                 self.proizvodi.remove(proizvod)
-                print("Proizvod je uspešno obrisan.")
                 return
-        print("Proizvod nije pronađen.")
-
-    # Funkcija za ažuriranje proizvoda
-    def update_proizvod(self):
-        try:
-            id_proizvoda = int(input("Unesite ID proizvoda za ažuriranje> ").strip())
-        except ValueError:
-            print("Greška: Unesite validan broj za ID.")
-            return
-
-        # Pronađi proizvod i prikaži trenutne podatke
-        # Da li postoji jednostavniji način da se pronađe proizvod? Možda koristiti dict umesto liste?
-        for proizvod in self.proizvodi:
-            if proizvod["id"] == id_proizvoda:
-                print(f"Trenutni podaci za {proizvod['naziv']}:")
-                print(f"Cena: {proizvod['cena']}")
-                print(f"Proizvođač: {proizvod['proizvodjac']}")
-
-                novi_naziv = input(f"Unesite novi naziv ({proizvod['naziv']})> ").strip()
-                if novi_naziv:
-                    proizvod["naziv"] = novi_naziv
-
-                nova_cena = input(f"Unesite novu cenu ({proizvod['cena']})> ").strip()
-                if nova_cena:
-                    try:
-                        proizvod["cena"] = float(nova_cena)
-                    except ValueError:
-                        print("Greška: Cena nije ažurirana jer uneta vrednost nije validan broj.")
-
-                novi_proizvodjac = input(f"Unesite novog proizvođača ({proizvod['proizvodjac']})> ").strip()
-                if novi_proizvodjac:
-                    proizvod["proizvodjac"] = novi_proizvodjac
-
-                print("Proizvod je uspešno ažuriran.")
-                return
-        print("Proizvod nije pronađen.")
-
-    # TODO move to model
+        raise ValueError(f"Proizvod - ID {id} nije pronađen.")
+    
+    def update_proizvod(self, id, naziv, cena, proizvodjac):
+        proizvod = self.get_proizvod_by_id(id)
+        proizvod['naziv'] = naziv
+        proizvod['cena'] = cena
+        proizvod['proizvodjac'] = proizvodjac
+    
     # TODO Preći na csv - standardna biblioteka biblioteka. Pitati na konsultacijama da li se može koristiti JSON
-    # Sačuvaj podatke u fajl
-    def save_data(self, file="proizvodi.txt"):
+    # Pitati na konsultacijama da li je potrebno obraditi slučaj kada vrednost sadrži yarey
+    def save_proizvodi(self, file='proizvodi.txt'):
         with open(file, "w", encoding='utf-8', ) as f:
             for proizvod in self.proizvodi:
                 f.write(f"{proizvod['id']},{proizvod['naziv']},{proizvod['cena']},{proizvod['proizvodjac']}\n")
-
-    # TODO move to model
-    # Učitaj podatke iz fajla
-    # Pitati na konsultacijama da li je potrebno obraditi slučaj kada vrednost sadrži yarey
+    
     def load_data(self, file="proizvodi.txt"):
         self.proizvodi = []  # resetovanje liste proizvoda pre učitavanja
         try:
@@ -204,8 +261,6 @@ class ProizvodController:
             exit(-1)
         except FileNotFoundError:
             print("Nije pronađen fajl sa podacima. Početak sa praznom listom proizvoda.")
-
-
 
 
 if __name__ == "__main__":
